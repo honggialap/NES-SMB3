d61983413de9820b05b380979c56c5d1cf1f0ada
Hong Gia Lap <laphg97@gmail.com>
2022-05-21 03:16:18 +0700

    * Last commit of branch - So now I'm gonna close up this branch, I know this is not testable for most of the components. - We gonna keep on working on world branch. - I promise I'm gonna clean up my styling after I get some actor running. Learning Java this semester messed up my styling :(

--- Model/SMB3.cpp
+++ Model/SMB3.cpp
@@ -1,6 +1,4 @@
-#pragma region INCLUDE
 #include "SMB3.h"
-#pragma endregion
 
 pGameObject CSMB3::Create(unsigned int actorID, std::string name, std::string source, float posX, float posY, int gridX, int gridY, unsigned int layer)
 {
